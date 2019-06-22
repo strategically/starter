2087d240957201677a82fb0b92e61f250ef6a099
Akinul Islam Jony <akinul@gmail.com>
2019-06-23 00:27:24 +0200

    change phone number

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,4 +1,4 @@
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 import React from "react"
 
 import phoneicon from "../images/phoneicon.png";
@@ -10,7 +10,7 @@ const LinkStyle = {
 
 const Navigation = () => (
     <div>
-        <a href="tel:+34612401232" style={ LinkStyle }>
+        <a href="tel:+447882090659" style={ LinkStyle }>
             <img src={phoneicon} alt="phone-icon" height="45" width="45"/>
         </a>
 
